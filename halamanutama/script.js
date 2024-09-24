31d564352298d167a504ca0b30a41a37b344e185
b4baxxx <160828060+b4baxxx@users.noreply.github.com>
2024-09-24 12:20:17 +0700

    abiyyu al thoriq zidan
    REVISI REGIS,JS,CSS

--- halamanutama/script.js
+++
@@ -1,8 +0,0 @@
-function verifyRegistration() {
-  var verificationCode = document.getElementById('verification-code').value;
-  
-  // Proses verifikasi kode
-  
-  // Jika verifikasi berhasil, redirect ke halaman pengisian KRS
-  window.location.href = "krs.html";
-}
